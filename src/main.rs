0389daabea5cc731776f817f68247a07c7b42e7f
likg227 <55606560+likg227@users.noreply.github.com>
2021-11-06 22:48:12 +0800

    Initialize ci

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+#[allow(clippy::float_cmp)]
+mod vec3;
+use image::{ImageBuffer, RgbImage};
+use indicatif::ProgressBar;
+use std::sync::mpsc::channel;
+use std::sync::Arc;
+use threadpool::ThreadPool;
+
+pub use vec3::Vec3;
+
+struct World {
+    pub height: u32,
+}
+
+impl World {
+    pub fn color(&self, _: u32, y: u32) -> u8 {
+        (y * 256 / self.height) as u8
+    }
+}
+
+fn main() {
+    let is_ci = match std::env::var("CI") {
+        Ok(x) => x == "true",
+        Err(_) => false,
+    };
+
+    let (n_jobs, n_workers): (usize, usize) = if is_ci { (32, 2) } else { (16, 2) };
+
+    println!(
+        "CI: {}, using {} jobs and {} workers",
+        is_ci, n_jobs, n_workers
+    );
+
+    let height = 512;
+    let width = 1024;
+
+    let (tx, rx) = channel();
+    let pool = ThreadPool::new(n_workers);
+
+    let bar = ProgressBar::new(n_jobs as u64);
+
+    let world = Arc::new(World { height });
+
+    for i in 0..n_jobs {
+        let tx = tx.clone();
+        let world_ptr = world.clone();
+        pool.execute(move || {
+            let row_begin = height as usize * i / n_jobs;
+            let row_end = height as usize * (i + 1) / n_jobs;
+            let render_height = row_end - row_begin;
+            let mut img: RgbImage = ImageBuffer::new(width, render_height as u32);
+            for x in 0..width {
+                for (img_y, y) in (row_begin..row_end).enumerate() {
+                    let y = y as u32;
+                    let pixel = img.get_pixel_mut(x, img_y as u32);
+                    let color = world_ptr.color(x, y);
+                    *pixel = image::Rgb([color, color, color]);
+                }
+            }
+            tx.send((row_begin..row_end, img))
+                .expect("failed to send result");
+        });
+    }
+
+    let mut result: RgbImage = ImageBuffer::new(width, height);
+
+    for (rows, data) in rx.iter().take(n_jobs) {
+        for (idx, row) in rows.enumerate() {
+            for col in 0..width {
+                let row = row as u32;
+                let idx = idx as u32;
+                *result.get_pixel_mut(col, row) = *data.get_pixel(col, idx);
+            }
+        }
+        bar.inc(1);
+    }
+
+    result.save("output/test.png").unwrap();
+    bar.finish();
+}
